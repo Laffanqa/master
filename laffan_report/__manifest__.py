100e869150576e19ab83146442bf8e515ff8e9de
Laffanqa <105169518+Laffanqa@users.noreply.github.com>
2022-06-04 00:28:31 +0530

    Add files via upload

---
+++ laffan_report/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Laffan Report",
+
+    'summary': """
+     Sales""",
+
+    'description': """
+        Customisation in quotation report
+    """,
+
+    'author': "Advanced Solutions",
+    'website': "http://www.advanced.qa",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.4',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'sale_management', 'project'],
+
+    # always loaded
+    'data': [
+        'views/views.xml',
+        'views/res_company.xml',
+        'report/report_quotation.xml',
+    ],
+}
